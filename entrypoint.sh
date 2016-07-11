67f0977e694b6ab00b39d9824ea7e3e8a75c12bf
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-07-11 12:34:22 +0200

    Composer token test 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ mkdir -p /var/app/var/logs
 chmod -R 0777 /var/app/var/cache
 chmod -R 0777 /var/app/var/logs
 
-composer config github-oauth.github.com "dmxjZWs6NjgwOGM3MzVkZDkwMDZjNjBiOWRmM2RjYjc5MTI5OGUwMjkxNjgzZg=="
+composer config github-oauth.github.com "9b41dc4199ceb4611598caa882e06115931d85f8"
 
 exec apache2-foreground
