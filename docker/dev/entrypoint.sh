fdc4f614dff30508ef1ac85b965eaf62bbd93a32
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-07-16 09:16:07 +0200

    Add supervisor

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash sh
 
-mkdir -p /var/app/var/cache
 mkdir -p /var/app/var/logs
 mkdir -p /var/app/web/compiled
+rm -Rf /var/app/var/cache/
+ln -s /dev/shm /var/app/var/cache
 
 RUN composer config -g github-oauth.github.com 9b41dc4199ceb4611598caa882e06115931d85f8
 
@@ -11,15 +12,15 @@ composer run-script post-install-cmd --no-interaction
 bin/console assetic:dump 
 
 chmod -R 0777 /var/app/var/logs
-chmod -R 0777 /var/app/var/cache
 chmod -R 0777 var/app/web/compiled
+chmod -R 0777 /dev/shm
 
 service postfix start
 
 service cron start
 
-chmod +x /var/app/docker/supervisor-manager.sh
-./docker/supervisor-manager.sh start
+supervisord -c vendor/modpreneur/venice/supervisor/supervisord.conf
+supervisorctl -c vendor/modpreneur/venice/supervisor/supervisord.conf status
 
 java -jar /opt/selenium-server-standalone.jar -role hub &
 phantomjs --webdriver=8080 &
