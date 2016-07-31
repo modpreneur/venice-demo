f67405d8a902e1e089fb1819a929633ff57abc0a
Jakub Fajkus <jakub.fajkus@gmail.com>
2016-07-31 17:26:30 +0200

    Add custom field to BlogArticleType

--- src/AppBundle/Form/BlogArticleType.php
+++ src/AppBundle/Form/BlogArticleType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\BlogArticle;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -11,6 +12,15 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class BlogArticleType extends \Venice\AppBundle\Form\BlogArticleType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('BlogArticleChild');
+    }
+
+
+
     /**
      * @param OptionsResolver $resolver
      * @throws \Symfony\Component\OptionsResolver\Exception\AccessException
