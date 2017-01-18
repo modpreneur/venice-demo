db92f9f78b2efb3776dd91441648772057bdfaf9
Jakub Fajkus <jakub.fajkus@gmail.com>
2017-01-18 14:33:22 +0100

    Add custom field "upsell order" to form in administration

--- src/AppBundle/Form/Product/StandardProductType.php
+++ src/AppBundle/Form/Product/StandardProductType.php
@@ -3,6 +3,8 @@
 namespace AppBundle\Form\Product;
 
 use AppBundle\Entity\Product\StandardProduct;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -10,6 +12,14 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
  */
 class StandardProductType extends \Venice\AppBundle\Form\Product\StandardProductType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('upsellOrder', IntegerType::class);
+    }
+
+
     /**
      * @param OptionsResolver $resolver
      *
