03a9161ac79f1cee70f75e8a58bc977397aa0e02
Martin Kolek <kolek@modpreneur.com>
2017-03-01 19:35:15 +0100

    remove composer config github-oauth.github.com from deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,7 +5,7 @@ mkdir -p /var/app/var/cache/prod
 mkdir -p /var/app/var/logs
 mkdir -p /var/app/var/logs/supervisord
 
-composer config github-oauth.github.com "9b41dc4199ceb4611598caa882e06115931d85f8"
+#composer config github-oauth.github.com "9b41dc4199ceb4611598caa882e06115931d85f8"
 composer install --no-scripts --no-suggest --optimize-autoloader
 
 rm -R /var/app/var/cache/prod/*
