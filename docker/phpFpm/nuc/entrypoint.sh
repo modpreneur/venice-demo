ebc44f0a6308c20769170bb3372073257d5e12c3
Fajkus <jakub.fajkus@gmail.com>
2017-03-17 16:11:09 +0100

    Fix entrypoints - newest composer.lock

--- docker/phpFpm/nuc/entrypoint.sh
+++ docker/phpFpm/nuc/entrypoint.sh
@@ -7,9 +7,9 @@ mkdir -p /var/app/var/logs/supervisord
 composer install --no-scripts --no-suggest --optimize-autoloader
 #composer run-script post-install-cmd --no-interaction
 
-#supervisor - load config from necktie
-ENV=prod supervisord -c /var/app/supervisor/supervisord.conf
-supervisorctl -c /var/app/supervisor/supervisord.conf reload
+#supervisor - load config from venic framework
+ENV=dev supervisord -c vendor/modpreneur/venice/supervisor/supervisord.conf
+supervisorctl -c vendor/modpreneur/venice/supervisor/supervisord.conf status
 
 chmod -R 0777 /var/app/var/cache
 chmod -R 0777 /var/app/var/logs
