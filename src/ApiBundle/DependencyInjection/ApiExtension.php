01d31a689c5c025634d00ccbf44046aba8b27d89
Marek Omasta <omasta@modpreneur.com>
2017-04-05 16:13:28 +0200

    Api bundle files

---
+++ src/ApiBundle/DependencyInjection/ApiExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ApiBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class FrontExtension
+ * @package FrontBundle\DependencyInjection
+ */
+class ApiExtension extends Extension
+{
+    /**
+     * Loads a specific configuration.
+     *
+     * @param array            $config    An array of configuration values
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     *
+     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     * @throws \Exception
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+    }
+}
